6cb9074d8f660540ab3288d74c3c61438d86527f
srcsereach <63546974+srcsereach@users.noreply.github.com>
2020-06-26 17:49:20 +0800

    代码

---
+++ Tool/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Tool
+{
+    public class Class1 : MonoBehaviour
+    {
+        void Star()
+        {
+            Debug.Log("qqqq");
+        }
+    }
+}
---
+++ Tool/GenericSingle.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericSingle<T> : MonoBehaviour where T: GenericSingle<T> ,new ()//where后面是对T的约束所以两个继承不会冲突
+{
+    private static T instance;
+
+    public static T Instance {
+        get
+        {
+
+            if(instance == null)
+            {
+                //查询有没有这个组件
+               
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    //‘"Single of"+typeof(T).ToString()’是给这个命名
+                    GameObject gameObject = new GameObject("Single of " + typeof(T).ToString(),typeof(T));
+                    instance = gameObject.GetComponent<T>();
+
+                }
+                else
+                {
+                    instance.gameObject.name = "Single of " + typeof(T).ToString();
+                }
+                instance.Init();
+                //保证场景切换，这个组件不会被销毁
+                GameObject.DontDestroyOnLoad(instance.gameObject);
+            }
+           
+            return instance;
+        }
+        set => instance = value;
+
+    }
+    //初始化场景
+    public virtual void Init()
+    {
+
+    }
+    //初始化查询物体--防止一开始就挂上脚本
+    private void Awake()
+    {
+
+        if (instance = null)
+        {
+            instance = this as T;
+            instance.Init();
+            GameObject.DontDestroyOnLoad(instance.gameObject);
+        }
+    }
+
+    //场景退出，销毁实例
+    private void OnApplicationQuit()
+    {
+        instance = null;
+    }
+}
